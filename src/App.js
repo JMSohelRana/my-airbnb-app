13100a92bf41577fed8721a49622d29f61a3f8ec
JMSohelRana <jmsohelrana04@gmail.com>
2023-12-30 20:14:27 +0600

    check all file working or not

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+// import img1 from './images/flowers.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <header>AirBnb</header>
+      <div>
+        {/* <img src={img1} alt="" /> */}
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
