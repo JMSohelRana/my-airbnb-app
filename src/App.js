a4cbdbd9a72a3aeb987ee27dafb04beb507dee2e
JMSohelRana <jmsohelrana04@gmail.com>
2023-12-30 20:25:45 +0600

    clear all unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 
-import './App.css';
-// import img1 from './images/flowers.jpg'
-
 function App() {
   return (
     <div className="App">
       <header>AirBnb</header>
       <div>
-        {/* <img src={img1} alt="" /> */}
-        <h1>hello</h1>
+       
+        <h1>hello boy</h1>
       </div>
     </div>
   );
