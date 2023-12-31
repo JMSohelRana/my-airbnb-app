fc49f476b20f5d70c64c50472d8298b617a4a6f5
JMSohelRana <jmsohelrana04@gmail.com>
2023-12-31 14:57:07 +0600

    navber all three section added without style and formatting

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import logo from "./logo.png";
+import { TbWorld } from "react-icons/tb";
+import { IoIosMenu } from "react-icons/io";
+import { FaUser } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="border-b flex items-center">
       {/* 1st part logo section */}
       <div className="flex h-20 w-20">
         {" "}
@@ -17,6 +20,24 @@ const Navbar = () => {
         </div>
       </div>
       {/* 2nd part middle section */}
+      <div className="flex ml-20">
+        <input type="search" placeholder="" />
+        <div>
+          <button>Anywhere</button>
+          <button>Any week</button>
+          <button>Add guests</button>
+        </div>
+      </div>
+
+      {/* 3rd part end section */}
+      <div className="flex items-center">
+        <p>Airbnb your home</p>
+        <TbWorld />
+      </div>
+      <div className="flex items-center">
+        <IoIosMenu />
+        <FaUser />
+      </div>
     </div>
   );
 };
