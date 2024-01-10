591e329a27fea9e10b3d24b4a582aa857ad1a9d4
JMSohelRana <jmsohelrana04@gmail.com>
2024-01-10 20:39:29 +0600

    create a new component named products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Filters from "./components/filters/Filters";
 import Navbar from "./components/navbar/Navbar";
+import Products from "./components/products/Products";
 
 function App() {
   return (
     <div className="bg-gray-40">
       <Navbar></Navbar>
       <Filters></Filters>
+      <Products></Products>
     </div>
   );
 }
