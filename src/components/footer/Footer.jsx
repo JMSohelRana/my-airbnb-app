2a583c536333f8dc3b03c7f403f1d2961a04b0a7
JMSohelRana <jmsohelrana04@gmail.com>
2024-01-15 01:27:50 +0600

    add footer community section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="">
         <div className="">
           <div className="">
-            <h3 className="">Explore</h3>
+            <h3 className="text-lg font-semibold mb-2">Explore</h3>
             <ul>
               <li>
                 <a href="home">About us</a>
@@ -16,6 +16,20 @@ const Footer = () => {
               </li>
               <li>
                 <a href="home">Press</a>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <h3 className="">Community</h3>
+            <ul>
+              <li>
+                <a href="home">Diversity & Belonging</a>
+              </li>
+              <li>
+                <a href="/">Accessibility</a>
+              </li>
+              <li>
+                <a href="home">Airbnb Associates</a>
               </li>
             </ul>
           </div>
