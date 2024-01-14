d882f209c803a3893d569ecdf6b0c908aad1d53b
JMSohelRana <jmsohelrana04@gmail.com>
2024-01-14 17:11:06 +0600

    add a clickable rating button with and warning sign. also give some style in rating button.make it flex with other elements

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-// import { Carousel } from "react-responsive-carousel";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { MdStarRate } from "react-icons/md";
+
 const Product = ({ product }) => {
   const { title, price, date, images } = product;
-
+  const handleRateClick = () => {
+    alert("Rating clicked for " + title);
+  };
   return (
     <div>
       <div className=" flex">
@@ -15,21 +17,26 @@ const Product = ({ product }) => {
               key={index}
               src={image}
               alt={`Product ${index + 1}`}
-              className="w-full h-32 object-cover mb-2"
+              className="w-full h-50  object-cover mb-2 rounded-lg"
             />
           ))}{" "}
         </div>
-        {/* <img
-          src={product.images.map()}
-          alt=""
-          className="object-cover rounded-[0.5rem]"
-        /> */}
       </div>
-      <div className="">
-        <p>{title}</p>
-        <span className="text-gray-500 font-light">${price} night</span>
-        <br />
-        <span>{date}</span>
+      <div className="flex justify-between">
+        <div className="">
+          <p className="font-semibold">{title}</p>
+          <span className="text-gray-600 font-light">${price} night</span>
+          <br />
+          <span>{date}</span>
+        </div>
+        <div className="">
+          <button
+            onClick={handleRateClick}
+            className="bg-gray-200 p-2 rounded hover:bg-red-400"
+          >
+            <MdStarRate />
+          </button>
+        </div>
       </div>
     </div>
   );
